016eda557e49b3d84bf58d591c7d16767099b7b3
Kashira2 <nekit.egorov@icloud.com>
2022-08-26 12:24:17 +0300

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Cube(int num)
+{
+    for (int index = 1; index <= num; index++)
+    {
+        Console.Write($"{index * index * index}, ");
+    }
+
+}
+    Console.WriteLine("Введите число");
+    int N =int.Parse(Console.ReadLine());
+    Cube(N);
